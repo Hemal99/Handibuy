ab8dcc12c9d113613c960d92c3657231aacbf0ab
Hemal <h.githmin@gmail.com>
2021-06-08 19:02:15 +0530

    updated

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 
+     const glider = document.querySelectorAll('.glider')
+
+     console.log(glider.length);
+
+     for (let i=0;i<glider.length;i++){
+ 
       window.addEventListener('load',function(){
-        document.querySelector('.glider').addEventListener('glider-slide-visible', function(event){
+        document.querySelectorAll('.glider')[i].addEventListener('glider-slide-visible', function(event){
             var glider = Glider(this);
             console.log('Slide Visible %s', event.detail.slide)
         });
-        document.querySelector('.glider').addEventListener('glider-slide-hidden', function(event){
+        document.querySelectorAll('.glider')[i].addEventListener('glider-slide-hidden', function(event){
             console.log('Slide Hidden %s', event.detail.slide)
         });
-        document.querySelector('.glider').addEventListener('glider-refresh', function(event){
+        document.querySelectorAll('.glider')[i].addEventListener('glider-refresh', function(event){
             console.log('Refresh')
         });
-        document.querySelector('.glider').addEventListener('glider-loaded', function(event){
+        document.querySelectorAll('.glider')[i].addEventListener('glider-loaded', function(event){
             console.log('Loaded')
         });
 
-        window._ = new Glider(document.querySelector('.glider'), {
-            slidesToShow: 3, //'auto',
+        window._ = new Glider(document.querySelectorAll('.glider')[i], {
+            slidesToShow: i===0?4:5, //'auto',
             slidesToScroll: 1,
             itemWidth: 150,
             draggable: true,
@@ -23,15 +29,15 @@
             dots: '#dots',
             rewind: true,
             arrows: {
-                prev: '.glider-prev',
-                next: '.glider-next'
+                prev: i===0?"":'.glider-prev',
+                next:i===0?"": '.glider-next'
             },
             responsive: [
                 {
                     breakpoint: 800,
                     settings: {
                         slidesToScroll: 'auto',
-                        itemWidth: 300,
+                        itemWidth: i===0?370:i===1?300:250,
                         slidesToShow: 'auto',
                         exactWidth: true
                     }
@@ -66,233 +72,8 @@
         });
       });
 
+     }
+     
+    
 
 
-
-      window.addEventListener("load", function () {
-        document
-          .querySelector(".glider-2")
-          .addEventListener("glider-slide-visible", function (event) {
-            var glider = Glider(this);
-            console.log("Slide Visible %s", event.detail.slide);
-          });
-        document
-          .querySelector(".glider-2")
-          .addEventListener("glider-slide-hidden", function (event) {
-            console.log("Slide Hidden %s", event.detail.slide);
-          });
-        document
-          .querySelector(".glider-2")
-          .addEventListener("glider-refresh", function (event) {
-            console.log("Refresh");
-          });
-        document
-          .querySelector(".glider-2")
-          .addEventListener("glider-loaded", function (event) {
-            console.log("Loaded");
-          });
-
-        window._ = new Glider(document.querySelector(".glider-2"), {
-          slidesToShow: 2, //'auto',
-          slidesToScroll: 1,
-          itemWidth: 150,
-          draggable: true,
-          scrollLock: false,
-          dots: "#dots",
-          rewind: true,
-          arrows: {
-            prev: ".glider-prev",
-            next: ".glider-next",
-          },
-          responsive: [
-            {
-              breakpoint: 800,
-              settings: {
-                slidesToScroll: "auto",
-                itemWidth: 300,
-                slidesToShow: "auto",
-                exactWidth: true,
-              },
-            },
-            {
-              breakpoint: 700,
-              settings: {
-                slidesToScroll: 4,
-                slidesToShow: 4,
-                dots: false,
-                arrows: false,
-              },
-            },
-            {
-              breakpoint: 600,
-              settings: {
-                slidesToScroll: 3,
-                slidesToShow: 3,
-              },
-            },
-            {
-              breakpoint: 300,
-              settings: {
-                slidesToScroll: 2,
-                slidesToShow: 1,
-                dots: false,
-                arrows: false,
-                scrollLock: true,
-              },
-            },
-          ],
-        });
-      });
-
-
-      
-      window.addEventListener("load", function () {
-        document
-          .querySelector(".glider-3")
-          .addEventListener("glider-slide-visible", function (event) {
-            var glider = Glider(this);
-            console.log("Slide Visible %s", event.detail.slide);
-          });
-        document
-          .querySelector(".glider-3")
-          .addEventListener("glider-slide-hidden", function (event) {
-            console.log("Slide Hidden %s", event.detail.slide);
-          });
-        document
-          .querySelector(".glider-3")
-          .addEventListener("glider-refresh", function (event) {
-            console.log("Refresh");
-          });
-        document
-          .querySelector(".glider-3")
-          .addEventListener("glider-loaded", function (event) {
-            console.log("Loaded");
-          });
-
-        window._ = new Glider(document.querySelector(".glider-3"), {
-          slidesToShow: 2, //'auto',
-          slidesToScroll: 1,
-          itemWidth: 150,
-          draggable: true,
-          scrollLock: false,
-          dots: "#dots",
-          rewind: true,
-          arrows: {
-            prev: ".glider-prev-3",
-            next: ".glider-next-3",
-          },
-          responsive: [
-            {
-              breakpoint: 800,
-              settings: {
-                slidesToScroll: "auto",
-                itemWidth: 300,
-                slidesToShow: "auto",
-                exactWidth: true,
-              },
-            },
-            {
-              breakpoint: 700,
-              settings: {
-                slidesToScroll: 4,
-                slidesToShow: 4,
-                dots: false,
-                arrows: false,
-              },
-            },
-            {
-              breakpoint: 600,
-              settings: {
-                slidesToScroll: 3,
-                slidesToShow: 3,
-              },
-            },
-            {
-              breakpoint: 300,
-              settings: {
-                slidesToScroll: 2,
-                slidesToShow: 1,
-                dots: false,
-                arrows: false,
-                scrollLock: true,
-              },
-            },
-          ],
-        });
-      });
-
-
-      window.addEventListener("load", function () {
-        document
-          .querySelector(".glider-4")
-          .addEventListener("glider-slide-visible", function (event) {
-            var glider = Glider(this);
-            console.log("Slide Visible %s", event.detail.slide);
-          });
-        document
-          .querySelector(".glider-4")
-          .addEventListener("glider-slide-hidden", function (event) {
-            console.log("Slide Hidden %s", event.detail.slide);
-          });
-        document
-          .querySelector(".glider-4")
-          .addEventListener("glider-refresh", function (event) {
-            console.log("Refresh");
-          });
-        document
-          .querySelector('.glider-4')
-          .addEventListener("glider-loaded", function (event) {
-            console.log("Loaded");
-          });
-
-        window._ = new Glider(document.querySelector('.glider-4'), {
-          slidesToShow: 2, //'auto',
-          slidesToScroll: 1,
-          itemWidth: 150,
-          draggable: true,
-          scrollLock: false,
-          dots: "#dots",
-          rewind: true,
-          arrows: {
-            prev: ".glider-prev-3",
-            next: ".glider-next-3",
-          },
-          responsive: [
-            {
-              breakpoint: 800,
-              settings: {
-                slidesToScroll: "auto",
-                itemWidth: 300,
-                slidesToShow: "auto",
-                exactWidth: true,
-              },
-            },
-            {
-              breakpoint: 700,
-              settings: {
-                slidesToScroll: 4,
-                slidesToShow: 4,
-                dots: false,
-                arrows: false,
-              },
-            },
-            {
-              breakpoint: 600,
-              settings: {
-                slidesToScroll: 3,
-                slidesToShow: 3,
-              },
-            },
-            {
-              breakpoint: 300,
-              settings: {
-                slidesToScroll: 2,
-                slidesToShow: 1,
-                dots: false,
-                arrows: false,
-                scrollLock: true,
-              },
-            },
-          ],
-        });
-      });
---
+++ index.js
@@ -0,0 +1,10 @@
+//https://handybuy.lk/
+
+
+const searchBtn = document.getElementsByClassName('searchButton')[0];
+
+
+
+searchBtn.addEventListener('click',()=>{
+    window.location.href = "https://handybuy.lk";
+})
